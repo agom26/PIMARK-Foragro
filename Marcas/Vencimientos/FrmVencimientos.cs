d252e12b06df4d06e4ffc3db477f64fdd3b6eb47
agom26 <gaby26@umes.edu.gt>
2024-11-03 19:23:01 -0600

    correos enviados

--- Marcas/Vencimientos/FrmVencimientos.cs
+++ Marcas/Vencimientos/FrmVencimientos.cs
@@ -23,7 +23,7 @@ namespace Presentacion.Vencimientos
         private async void FrmVencimientos_Load(object sender, EventArgs e)
         {
             await Task.Run(() => LoadVencimientos());
-            AgregarBotonEnviarCorreo(); // Agregar el botón después de cargar los datos
+            AgregarBotonEnviarCorreo(); 
         }
 
         private void LoadVencimientos()
@@ -70,16 +70,13 @@ namespace Presentacion.Vencimientos
             {
                 e.Handled = true;
 
-                // Pinta el fondo del botón
                 using (Brush brush = new SolidBrush(ColorTranslator.FromHtml("#34918a")))
                 {
                     e.Graphics.FillRectangle(brush, e.CellBounds);
                 }
 
-                // Dibuja el texto en blanco
                 TextRenderer.DrawText(e.Graphics, e.Value?.ToString(), e.CellStyle.Font, e.CellBounds, Color.White, TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
 
-                // Dibuja el borde del botón
                 e.Graphics.DrawRectangle(Pens.Black, e.CellBounds);
             }
         }
@@ -91,22 +88,25 @@ namespace Presentacion.Vencimientos
         {
             if (e.ColumnIndex == dtgVencimientos.Columns["EnviarCorreo"].Index && e.RowIndex >= 0)
             {
-                if (isSendingEmail) return; 
+                if (isSendingEmail) return;
 
-                isSendingEmail = true; 
+                isSendingEmail = true;
                 var filaSeleccionada = dtgVencimientos.Rows[e.RowIndex];
 
                 if (filaSeleccionada.DataBoundItem is DataRowView dataRowView)
                 {
                     int marcaId = Convert.ToInt32(dataRowView["marcaID"]);
                     var user = vencimientoModel.GetCorreosPorMarcaId(marcaId);
+                    string tipo = dataRowView["tipo"].ToString();  // Obtener el tipo
+                    DateTime fechaVencimiento = Convert.ToDateTime(dataRowView["Vencimiento"]);
+                    string nombre= dataRowView["Nombre"].ToString();
 
                     if (!string.IsNullOrEmpty(user.CorreoAgente))
                     {
                         try
                         {
                             dtgVencimientos.Enabled = false;
-                            await Task.Run(() => EnviarCorreo(user.CorreoAgente));
+                            await Task.Run(() => EnviarCorreo(user.CorreoAgente,nombre, tipo, fechaVencimiento));
                         }
                         catch (Exception ex)
                         {
@@ -122,11 +122,12 @@ namespace Presentacion.Vencimientos
                         MessageBox.Show("No hay un correo registrado para este agente.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     }
                 }
-                isSendingEmail = false; // Marcar como finalizado
+                isSendingEmail = false;
             }
         }
 
-        public void EnviarCorreo(string receptor)
+
+        public async void EnviarCorreo(string receptor,string nombre, string tipo, DateTime fechaVencimiento)
         {
             try
             {
@@ -134,13 +135,29 @@ namespace Presentacion.Vencimientos
                 message.From.Add(new MailboxAddress("Berger Pemueller", "avisos@bpa.com.es"));
                 message.To.Add(new MailboxAddress("Destinatario", receptor));
                 message.Subject = "Avisos de vencimiento de Berger Pemueller";
-                message.Body = new TextPart("plain") { Text = "Su marca o patente ya va a vencer." };
+
+                
+                string mensajeCuerpo;
+                if (tipo.Equals("patente", StringComparison.OrdinalIgnoreCase))
+                {
+                    mensajeCuerpo = $"La patente {nombre} está próxima a vencer, la fecha de vencimiento es: {fechaVencimiento.ToShortDateString()}.";
+                }
+                else if (tipo.Equals("marca", StringComparison.OrdinalIgnoreCase))
+                {
+                    mensajeCuerpo = $"La marca {nombre} está próxima a vencer, la fecha de vencimiento es: {fechaVencimiento.ToShortDateString()}.";
+                }
+                else
+                {
+                    mensajeCuerpo = "Su marca o patente está próxima a vencer.";
+                }
+
+                message.Body = new TextPart("plain") { Text = mensajeCuerpo };
 
                 using (var client = new SmtpClient())
                 {
                     client.Connect("mail.bpa.com.es", 465, true);
                     client.Authenticate("avisos@bpa.com.es", "Berger*Pemueller*2024");
-                    client.Send(message);
+                    await client.SendAsync(message);
                     client.Disconnect(true);
                     MessageBox.Show("Correo enviado con éxito.");
                 }
@@ -150,5 +167,6 @@ namespace Presentacion.Vencimientos
                 Console.WriteLine($"Error al enviar el correo: {ex.Message}");
             }
         }
+
     }
 }
